8f525c0cafbe3855729dad63aeb3fc0feffbb26c
Lucas Melo <lucasmelo.tec.saude@gmail.com>
2025-05-14 10:23:18 -0300

    Fist commit, without generics

---
+++ LearningGenerics/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using LearningGenerics.Services;
+
+namespace LearningGenerics {
+    class Program {
+        public static void Main(string[] args) {
+            PrintService print = new PrintService();
+            Console.Write("How many values? ");
+            int values = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Type the values: ");
+            for (int i = 0; i < values; i++) {
+                int value = int.Parse(Console.ReadLine());
+                print.AddValue(value);
+            }
+            print.Print();
+            Console.WriteLine("First: " + print.First());
+        }
+    }
+}
